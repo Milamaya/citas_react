f9d322bd5c4316c1f5d28f4cf9dfe6c5df7c4c3f
Camila Sanchez <camila.cimaverso@gmail.com>
2023-11-30 12:47:22 -0500

    No fue mucho trabajo pero fue un trabajo honesto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header(){
     return(
         <header className="font-black text-5xl text-center md:w-2/3 mx-auto">        
             <h1>
-                Seguimiento Pacientes {''} 
+                Seguimiento Pacientes {''} Con Githun {' '}
                 <span className="text-indigo-600">Veterinaria</span>
             </h1> 
         </header>
